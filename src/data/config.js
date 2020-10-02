d482d4bd000d47a17bf5179b90e2d0036c880aea
Mikhail Garbuzov <madmxg@gmail.com>
2020-10-02 14:16:42 +0300

    Fixes vhost

--- src/data/config.js
+++ src/data/config.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const vhost = process.env.NODE_ENV === 'production' ? '/moscowjs.org' : '';
+// const vhost = process.env.NODE_ENV === 'production' ? '/moscowjs.org' : '';
+const vhost = '';
 
 module.exports = function () {
   return {
