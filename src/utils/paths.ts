27faa01ad1ce2fd1c9dda4016ca4023c179c527d
Дмитрий Королев <dima@MacBook-Pro-Dmitrij.local>
2021-09-30 23:35:29 +0300

    remove soft and hard signs from slugs

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,7 +1,7 @@
 import slugify from "slugify"
 
 const strictSlugify = (path: string) => slugify(path, {
-  remove: /[*+~.()'"!:@]/g
+  remove: /[*+~.()'"!:@ьъ]/g
 })
 
 export const eventPath = (slug: string) =>
